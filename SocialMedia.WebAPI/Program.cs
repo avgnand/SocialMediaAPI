f5305de0de3713343fa3a7971066d34fdcef6b34
Nick Andresen <nimandresen@gmail.com>
2022-07-12 14:53:19 -0400

    Add debug files and Create DbContext class; Setup connection strings

--- SocialMedia.WebAPI/Program.cs
+++ SocialMedia.WebAPI/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using SocialMedia.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Different connection strings for each member
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnectionNick");
+// var connectionString = builder.Configuration.GetConnectionString("DefaultConnectionZach");
+// var connectionString = builder.Configuration.GetConnectionString("DefaultConnectionMary");
 
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
