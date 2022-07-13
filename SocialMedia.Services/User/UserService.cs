df9e44cbef37dd0b86d9eaa30795b0874369a274
Zach Dearmin <zacharydearmin@gmail.com>
2022-07-13 10:25:36 -0400

    Other CRUD methods framework build out for the UserService

--- SocialMedia.Services/User/UserService.cs
+++ SocialMedia.Services/User/UserService.cs
@@ -77,23 +77,53 @@ namespace SocialMedia.Services.User
 
         // * GET User by email
         // public async Task<UserEntity> GetUserByEmailAsync(string email) {
-            // return await _context.Users.FirstOrDefaultAsync(user => user.Email.ToLower() == email.ToLower());
+        // return await _context.Users.FirstOrDefaultAsync(user => user.Email.ToLower() == email.ToLower());
         // }
 
         // * GET User by username
         // public async Task<UserEntity> GetUserByUsernameAsync(string username) {
-            // return await _context.Users.FirstOrDefaultAsync(user = user.Username.ToLower() == username.ToLower());
+        // return await _context.Users.FirstOrDefaultAsync(user = user.Username.ToLower() == username.ToLower());
         // }
 
         // * PUT Update a user
         // public async Task<bool> UpdateUserAsync(UserUpdate request) {
-            // // Find the user and validate it's owned by the user
-            // var userEntity = await _dbContext
+        // // Find the user and validate it's owned by the user
+        // var userEntity = await _dbContext.Users.FindAsync(request.Id);
+
+        // // By using the null conditional operator we can check if it's null at the same time we check the OwnerId
+        // // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/member-access-operators#null-conditional-operators--and-
+        // if (userEntity?.Id != _userId)
+        // {
+        // return false;
+        // }
+
+        // // Now update the entity's properties
+        // userEntity.Username = request.Username;
+        // userEntity.Email = request.Email;
+        // userEntity.FirstName = request.FirstName;
+        // userEntity.LasyName = request.Lastname;
+
+        // // Save the changes to the database
+        // var numberOfChanges = await _dbContext.SaveChangesAsync();
+
+        // // numberOfChanges is stated to be equal to 1 because only one row is updated
+        // return numberOfChanges == 1;
         // }
 
         // * DELETE Delete a user
-        // public async Task<bool> DeleteUserAsync() {
+        // public async Task<bool> DeleteUserAsync(int userId) {
+        // // Fint the user bt the given Id
+        // var userEntity = await _dbContext.Users.FindAsync(userId);
 
+        // // Validate the user exists and is owned by the user
+        // if (userEntity?.Id != _userId)
+        // {
+        // return false;
+        // }
+
+        // // Remove the user from the DbContext and assert that the one change was saved
+        // _dbContext.Users.Remove(userEntity);
+        // return await _dbContext.SaveChangesAsync() == 1;
         // }
     }
 }
