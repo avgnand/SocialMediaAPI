7e907f1c8f9679ed3de1b45fba859e444981961d
Zach Dearmin <zacharydearmin@gmail.com>
2022-07-13 10:15:39 -0400

    RegisterUser and GetUserById added to services layer

--- SocialMedia.Services/User/IUserService.cs
+++ SocialMedia.Services/User/IUserService.cs
@@ -1,9 +1,15 @@
+// TODO Add using statment for Task and UserRegister  
+using System.Threading.Tasks;
+using SocialMedia.Models.User;
+using SocialMedia.Data.Entities;
+
 namespace SocialMedia.Services.User
 {
     public interface IUserService
     {
-        Task<bool> RegisterUserAsync();
-        Task<> GetUserByIdAsync();
+        // TODO Add in method
+        Task<bool> RegisterUserAsync(UserRegister model);
+        Task<UserEntity> GetUserByIdAsync();
         // Task<> GetUserByUsernameAsync();
         // Task<bool> UpdateUserAsync();
         // Task<bool> DeleteUserAsync();
--- SocialMedia.Services/User/UserService.cs
+++ SocialMedia.Services/User/UserService.cs
@@ -1,31 +1,94 @@
+// TODO Double check using statements
+using System;
+using System.ComponentModel.DataAnnotations;
+uisng System.Threading.Tasks;
 using SocialMedia.Data;
+using SocialMedia.Data.Entities;
+using SocialMedia.Models.User;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace SocialMedia.Services.User
 {
     public class UserService : IUserService
     {
         private readonly AppDbContext _context;
-        public UserService(AppDbContext context) {
+        public UserService(AppDbContext context)
+        {
             _context = context;
         }
-        // * POST Create a user - required
-        public async Task<bool> RegisterUserAsync() {
 
+        // * POST Create a user - required
+        // Add in method
+        public async Task<bool> RegisterUserAsync(UserRegister model)
+        {
+            // Check values from methods for invalid data
+            if (await GetUserByIdAsync(model.Id) != null)
+            {
+                return false;
+            }
+            // Declare new UserEntity and assign values
+            var entity = new UserEntity
+            {
+                Email = model.Email,
+                Username = model.Username,
+                Password = model.Password,
+                DateCreated = DateTime.Now
+            };
+
+            // Hash password entered by user
+            var passwordHasher = new PasswordHasher<UserEntity>();
+
+            entity.Password = passwordHasher.HashPassword(entity, model.Password);
+
+            // Add entity to _context.Users DbSet
+            _context.Users.Add(entity);
+            // Save changes made to the DbSet
+            var numberOfChanges = await _context.SaveChangesAsync();
+
+            // Check to see if the user was registered
+            return numberOfChanges == 1;
         }
 
         // * GET User by id - required
-        public async Task<> GetUserByIdAsync() {
+        // Add in method
+        public async Task<UserDetail> GetUserByIdAsync(int userId)
+        {
+            // Check to see if user exists
+            var entity = await _context.Users.FindAsync(userId);
+            if (entity is null)
+            {
+                return null;
+            }
 
+            // Information to be display if user is found
+            var userDetail = new UserDetail
+            {
+                Id = entity.Id,
+                Email = entity.Email,
+                Username = entity.Username,
+                FirstName = entity.FirstName,
+                LastName = entity.LastName,
+                DateCreated = entity.DateCreated
+            };
+
+            return userDetail;
         }
 
-        // * GET User by username
-        // public async Task<> GetUserByUsernameAsync() {
+        // * GET User by email
+        // public async Task<UserEntity> GetUserByEmailAsync(string email) {
+            // return await _context.Users.FirstOrDefaultAsync(user => user.Email.ToLower() == email.ToLower());
+        // }
 
+        // * GET User by username
+        // public async Task<UserEntity> GetUserByUsernameAsync(string username) {
+            // return await _context.Users.FirstOrDefaultAsync(user = user.Username.ToLower() == username.ToLower());
         // }
 
         // * PUT Update a user
-        // public async Task<bool> UpdateUserAsync() {
-
+        // public async Task<bool> UpdateUserAsync(UserUpdate request) {
+            // // Find the user and validate it's owned by the user
+            // var userEntity = await _dbContext
         // }
 
         // * DELETE Delete a user
